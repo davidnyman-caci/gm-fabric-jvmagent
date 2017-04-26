5be87de2c7bf396a471529651154d7dd608c2122
Jeff Hemminger <jeff.hemminger@deciphernow.com>
2017-04-26 11:36:50 -0400

    resolves #12: corrected default path to application.conf

--- server/src/main/scala/com/deciphernow/gm/agent/Config.scala
+++ server/src/main/scala/com/deciphernow/gm/agent/Config.scala
@@ -19,4 +19,4 @@ import com.twitter.app.GlobalFlag
 object requestTimeout extends GlobalFlag[Int](10,"Override the request timeout for the client")
 object maxResponseSize extends GlobalFlag[Int](5,"Override the response size in MB for the client")
 object maxRequestSize extends GlobalFlag[Int](5,"Override the request size in MB for the client")
-object routeConfig extends GlobalFlag[String]("/etc/application.conf", "Override the default route configuration location")
+object routeConfig extends GlobalFlag[String]("etc/application.conf", "Override the default route configuration location")
